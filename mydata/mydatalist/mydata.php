9396f217183f147f4bb5de00037d33f473028abf
sonikf <93765174+sonikf@users.noreply.github.com>
2023-12-20 01:01:09 +0200

    ADD QR+invoiceUid thanks @amdam

--- mydata/mydatalist/mydata.php
+++ mydata/mydatalist/mydata.php
@@ -104,7 +104,7 @@ if (MYDATA_SUPPORT_MULTILINE == true){
 	{
 
 		$subprod_rang = preg_replace('/\D/', '', $sub_row["rang"]);
-                $subprod_desc = substr(html($sub_row["description"]),0,254);
+		$subprod_desc = substr(html($sub_row["description"]),0,254);
 		$subprod_net = abs(number_format($sub_row["subprod_net"],2,'.',''));
 		$subprod_tax_percent = $sub_row["subprod_tax_percent"];
 		$subprod_tax = abs(number_format($sub_row["subprod_tax"],2,'.',''));
@@ -214,6 +214,7 @@ try
 	//$mydata = $response->getBody();
 
 	$xmlstr = $response->getBody();
+	echo $xmlstr;
 	$xmlreply = new SimpleXMLElement($xmlstr);
 	$datetime = date("y/m/d H:i"); 
 
@@ -223,6 +224,8 @@ try
 		{
 			$mydata = $datetime ." [". $xmlreply->response[0]->invoiceMark ."]" ;
 			$mydata_check = 1;
+			$mydata_QR = $xmlreply->response[0]->qrUrl;
+			$mydata_invoiceUid = $xmlreply->response[0]->invoiceUid;
 			$update_query = "UPDATE ".$dolibarr_main_db_prefix."facture left join ".$dolibarr_main_db_prefix."facture_extrafields on ".$dolibarr_main_db_prefix."facture_extrafields.fk_object = ".$dolibarr_main_db_prefix."facture.rowid set mydata_reply ='$mydata', mydata_check='1' where ".$dolibarr_main_db_prefix."facture.rowid=" . $rowid;
 			$update_result = mysqli_query($con, $update_query);
 
@@ -231,7 +234,7 @@ try
 			$mydata = $datetime ." " . $xmlreply->response[0]->statusCode ." ". $xmlreply->response[0]->errors[0]->error[0]->message;
 			//$mydata = $response->getBody();
 			$mydata_check =0;
-			$update_query = "UPDATE ".$dolibarr_main_db_prefix."facture left join ".$dolibarr_main_db_prefix."facture_extrafields on ".$dolibarr_main_db_prefix."facture_extrafields.fk_object = ".$dolibarr_main_db_prefix."facture.rowid set mydata_reply ='$mydata' where ".$dolibarr_main_db_prefix."facture.rowid=" . $rowid;
+			$update_query = "UPDATE ".$dolibarr_main_db_prefix."facture left join ".$dolibarr_main_db_prefix."facture_extrafields on ".$dolibarr_main_db_prefix."facture_extrafields.fk_object = ".$dolibarr_main_db_prefix."facture.rowid set mydata_reply ='$mydata', mydata_reply_QR ='$mydata_QR', mydata_reply_invoiceUid = '$mydata_invoiceUid', mydata_check='1' where ".$dolibarr_main_db_prefix."facture.rowid=" . $rowid;
 			$update_result = mysqli_query($con, $update_query);
 		}
 
