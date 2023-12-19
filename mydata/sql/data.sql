27afc8e45740d1d373746e379f53aee2eaaf4c0e
sonikf <93765174+sonikf@users.noreply.github.com>
2023-12-20 01:03:24 +0200

    ADD QR+invoiceUid thanks @amdam

--- mydata/sql/data.sql
+++ mydata/sql/data.sql
@@ -9,7 +9,7 @@ INSERT INTO llx_c_paiement (code, libelle, type) VALUES ('7', 'POS / e-POS', 2);
 INSERT INTO llx_facture_extrafields (fk_object) select llx_facture.rowid from llx_facture left join llx_facture_extrafields on llx_facture_extrafields.fk_object = llx_facture.rowid where COALESCE(fk_object,0) = 0;
 
 -- UNCOMMENT BELOW LINE TO REMOVE ALL MYDATA DONE RECORDS. CLICK DISABLE AND ENABLE MODULE TO APPLY - REMEMBER TO COMMENT IT AGAIN AFTER ENABLING THE MODULE
---UPDATE llx_facture_extrafields SET mydata_check='',mydata_reply='';
+--UPDATE llx_facture_extrafields SET mydata_check='',mydata_reply='', mydata_reply_QR='', mydata_reply_invoiceUid='';
 
 
 
